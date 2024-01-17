4093493e58190c32328a264d09e4377186af5283
abolfazl <abolfazlrasouli95@gmail.com>
2024-01-17 13:02:02 +0330

    Making related settings for images in setting and url config. for the pillow package

--- config/settings.py
+++ config/settings.py
@@ -126,3 +126,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+AUTH_USER_MODEL = 'core.CustomUser'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = str(BASE_DIR.joinpath('media'))
