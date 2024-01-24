3b94a8fd61ca4f6b1a8e66ba10425b8189805d2e
abolfazl <abolfazlrasouli95@gmail.com>
2024-01-24 11:37:07 +0330

    Complete login view

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -1,10 +1,42 @@
 from django.urls import path
-from .views import LoginView, SignUpView
+from .views import UsernameLoginView
 
 
 app_name = "app_account"
 
+# urlpatterns = [
+#     path("login/",   LoginView.as_view(),   name="login_page"),
+#     path("signup/",  SignUpView.as_view(),  name="signup_page"),
+# ]
+
+
+
+
 urlpatterns = [
-    path("login/",   LoginView.as_view(),   name="login_page"),
-    path("signup/",  SignUpView.as_view(),  name="signup_page"),
+    # path(
+    #     'signup/',
+    #     SignupView.as_view(),
+    #     name='signup',
+    # ),
+    path(
+        'login/',
+        UsernameLoginView.as_view(),
+        name='login',
+    ),
+    # path(
+    #     'email/',
+    #     EmailLoginView.as_view(),
+    #     name='email',
+    # ),
+    # path(
+    #     'email/otp/',
+    #     OTPView.as_view(),
+    #     name='otp',
+    # ),
+    # path(
+    #     'logout/',
+    #     CustomLogoutView.as_view(),
+    #     name='logout',
+    # ),
+    # path('profile/<int:pid>/', Profile.as_view(), name='profile'),
 ]
--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -1,18 +1,18 @@
-from django.shortcuts import render
-from django.views import View
-from django.shortcuts import render, redirect
-from django.contrib.auth.views import LoginView
-from .forms import LoginForm, SignUpForm
-from django.urls import reverse_lazy
-from django.contrib.auth.decorators import user_passes_test
-from django.utils.decorators import method_decorator
-from django.views.generic.edit import CreateView
-from django.contrib.auth.decorators import login_required
-from django.utils.decorators import method_decorator
-from django.views.generic import DetailView
-from django.shortcuts import get_object_or_404
-from django.contrib.auth import login, logout
-from django.contrib.auth import get_user_model
+# from django.shortcuts import render
+# from django.views import View
+# from django.shortcuts import render, redirect
+# from django.contrib.auth.views import LoginView
+# from .forms import LoginForm, SignUpForm
+# from django.urls import reverse_lazy
+# from django.contrib.auth.decorators import user_passes_test
+# from django.utils.decorators import method_decorator
+# from django.views.generic.edit import CreateView
+# from django.contrib.auth.decorators import login_required
+# from django.utils.decorators import method_decorator
+# from django.views.generic import DetailView
+# from django.shortcuts import get_object_or_404
+# from django.contrib.auth import login, logout
+# from django.contrib.auth import get_user_model
 
 
 # class LoginView(LoginView):
@@ -41,3 +41,55 @@ from django.contrib.auth import get_user_model
 #         logout(request)
 #         return redirect("app_home:home_page")
 
+
+from django.contrib.auth import get_user_model
+from .forms import EmailCheckForm, LoginForm
+from .backends import CustomModelBackend as CMB
+from .utils import store_otp, check_otp
+from .tasks import send_otp_by_email
+
+from django.views import View
+
+from django.views.generic.edit import CreateView, FormView
+
+from django.contrib.auth import login, logout
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.views import LogoutView, LoginView
+
+from django.contrib.auth.decorators import login_required
+
+from django.core.exceptions import ValidationError
+from django.utils.translation import gettext_lazy as _
+from django.utils.decorators import method_decorator
+from django.shortcuts import render, redirect, HttpResponse
+from django.contrib import messages
+from django.urls import reverse_lazy
+
+
+User = get_user_model()
+
+
+class UsernameLoginView(LoginView):
+    form_class = LoginForm
+    template_name = 'accounts/login.html'
+    redirect_authenticated_user = True
+
+    def get_success_url(self):
+        return self.request.GET.get("next", reverse_lazy("home"))
+
+    # def get_success_url(self):
+    #     username = self.request.POST.get("username")
+    #     first_name = User.objects.get(username=username).first_name
+    #
+    #     messages.success(
+    #         self.request,
+    #         _(
+    #             f"Login Successfuly. " \
+    #             f"Welcome {first_name if first_name else username}."
+    #         )
+    #     )
+
+        # next_url = self.request.GET.get('next')
+        # if next_url:
+        #     return next_url
+        # return reverse_lazy('core:home')
