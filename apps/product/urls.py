4eefec519488d7dad14c333ef48456f1851d4a97
abolfazl <abolfazlrasouli95@gmail.com>
2024-01-31 16:23:17 +0330

    Implementation of discounts for category, product

--- apps/product/urls.py
+++ apps/product/urls.py
@@ -1,13 +1,14 @@
 
 from django.urls import path
-from .views import ProductDetailView, HomePage, CategoryDetailView, CommentCreateView, item_search
+from .views import ProductDetailView, HomePage, CategoryProductListView, CommentCreateView, item_search, LikeProductView
 
 app_name = "product"
 urlpatterns = [
     path('', HomePage.as_view(), name='home_page'),
-    path('category/<int:pk>/', CategoryDetailView.as_view(), name='category_product'),
+    path('category/<int:pk>/', CategoryProductListView.as_view(), name='category_product'),
     path('detail/<int:pk>/', ProductDetailView.as_view(), name='detail_product'),
     path('comment/<int:details_id>/', CommentCreateView.as_view(), name='comment_create'),
     path('search/', item_search, name='item_search'),
+    path('like/<int:pk>/', LikeProductView.as_view(), name='like_product')
 
 ]
--- apps/product/views.py
+++ apps/product/views.py
@@ -2,42 +2,66 @@ from django.shortcuts import render
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
 from django.views.generic import ListView, DetailView, DeleteView, UpdateView, TemplateView, CreateView
+from django.views import View
 from django.db.models import Q, F
 from .models import Product, Attribute, Discount, Category, Image, Comment, Like
 from .forms import CommentForm
 from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
-
+from django.http import JsonResponse
+from django.utils.translation import gettext_lazy as _
+from django.views.decorators.csrf import csrf_exempt
 
 class HomePage(ListView):
     model = Product
     template_name = 'product/home_product.html'
     context_object_name = 'products'
-    # paginate_by = 1
+    paginate_by = 2
     queryset = Product.objects.prefetch_related('images')
     print(queryset)
 
 
-class CategoryDetailView(DetailView):
-    model = Category
+# class CategoryDetailView(DetailView):
+#     model = Category
+#     template_name = 'product/category_product.html'
+#     paginate_by = 2
+#     context_object_name = 'categories'
+#
+#     def get_context_data(self, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#         category = self.get_object()
+#         print(category, 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa')
+#         subcategory = Category.objects.filter(replay_cat=category)
+#         print(subcategory,'bbbbbbbbbbbbbbbbbbbbbbbbbbbb')
+#         if subcategory.exists():
+#             products = Product.objects.filter(category__in=subcategory)
+#             # context['subcategory'] = subcategory
+#             print(products, 'cccccccccccccccccccccccccccccccccccccccccc')
+#         else:
+#             products = Product.objects.filter(category=category)
+#             print(products, 'dddddddddddddddddddddddddddddddddddddddddddd')
+#         context['products'] = products
+#         print(context, 'e' * 100)
+#         return context
+
+
+class CategoryProductListView(ListView):
     template_name = 'product/category_product.html'
-    context_object_name = 'categories'
+    context_object_name = 'products'
+    paginate_by = 2
+
+    def get_queryset(self):
+        cat = Category.objects.get(pk=self.kwargs['pk'])
+        subcategories = Category.objects.filter(replay_cat=cat)
+        if subcategories.exists():
+            queryset = Product.objects.filter(category__in=subcategories)
+        else:
+            queryset = Product.objects.filter(category=cat)
+        return queryset
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        category = self.get_object()
-        print(category, 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa')
-        subcategory = Category.objects.filter(replay_cat=category)
-        print(subcategory,'bbbbbbbbbbbbbbbbbbbbbbbbbbbb')
-        if subcategory.exists():
-            products = Product.objects.filter(category__in=subcategory)
-            # context['subcategory'] = subcategory
-            print(products, 'cccccccccccccccccccccccccccccccccccccccccc')
-        else:
-            products = Product.objects.filter(category=category)
-            print(products, 'dddddddddddddddddddddddddddddddddddddddddddd')
-        context['products'] = products
-        print(context, 'e' * 100)
+        context['cat'] = Category.objects.get(pk=self.kwargs['pk'])
         return context
 
 
@@ -55,8 +79,10 @@ class ProductDetailView(DetailView):
         # context['attributes'] = self.object.attribute.all()
         context['comments'] = comments
         context['images'] = product.images.all()
-        context['attributes'] = product.attribute.all()
+        # context['attributes'] = product.attribute.all()
         context['comment_form'] = CommentForm()
+        context['sizes'] = product.attribute.values_list('size', flat=True).distinct()
+        context['colors'] = product.attribute.values_list('code_color', flat=True).distinct()
 
         # print(self.object)
         # print(self.object.price)
@@ -82,8 +108,19 @@ class CommentCreateView(LoginRequiredMixin, CreateView):
         return super().form_valid(form)
 
 
-
 def item_search(request):
     search_query = request.GET.get('search')
     search = Product.objects.filter(Q(name__icontains=search_query) | Q(description__icontains=search_query) | Q(category__name__icontains=search_query))
     return render(request, 'product/category_product.html', {'products': search})
+
+
+class LikeProductView(LoginRequiredMixin, View):
+
+    def get(self, request, *args, **kwargs):
+        product_id = self.kwargs['pk']
+        user = request.user
+        if Like.objects.filter(product_id=product_id, user=user).exists():
+            return JsonResponse({'error': _('You have already liked this product.')}, status=400)
+
+        Like.objects.create(product_id=product_id, user=user)
+        return JsonResponse({'message': _('product liked')})
