ef702d1d098f22af83f29d1357674237a87a24e4
abolfazl <abolfazlrasouli95@gmail.com>
2024-02-01 22:27:24 +0330

    Implementing new products query in the HomePage class for the product app

--- apps/product/views.py
+++ apps/product/views.py
@@ -20,6 +20,11 @@ class HomePage(ListView):
     queryset = Product.objects.prefetch_related('images')
     print(queryset)
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['new_products'] = Product.objects.prefetch_related('images').order_by('-create_at')[:10]
+        return context
+
 
 # class CategoryDetailView(DetailView):
 #     model = Category
