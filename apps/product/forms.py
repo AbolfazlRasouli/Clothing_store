21941420ee039e7b85fa6487e04d1667e61d8877
abolfazl <abolfazlrasouli95@gmail.com>
2024-02-02 05:13:32 +0330

    Perform migration and place safe for html tags

--- apps/product/forms.py
+++ apps/product/forms.py
@@ -4,3 +4,6 @@ class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ('body', 'reply_comment')
+        widgets = {
+            'body': forms.Textarea(attrs={'cols': 100, 'rows': 10, 'placeholder': 'نظر خود را وارد کنید...'}),
+        }
