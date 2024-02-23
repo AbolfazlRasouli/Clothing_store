6d2fbfec596bc16db30b07727d42c5cd901452a4
abolfazl <abolfazlrasouli95@gmail.com>
2024-02-23 20:10:23 +0330

    Implementation of shopping cart template url to get information from serializer for orderitem model

--- apps/order/urls.py
+++ apps/order/urls.py
@@ -1,8 +1,9 @@
 
 from django.urls import path
-from .views import cart_view
+from .views import cart_view, show_buy
 
 app_name = "order"
 urlpatterns = [
-    path('cart/', cart_view, name='cart_view')
+    path('cart/', cart_view, name='cart_view'),
+    path('showbuy', show_buy, name='show_buy')
 ]
