5327d75f423b8bc25961aa65f6bdd9024fa4b765
abolfazl <abolfazlrasouli95@gmail.com>
2024-02-23 20:06:39 +0330

    Implementation of url serializer for orderitem model

--- apps/order/api/urls.py
+++ apps/order/api/urls.py
@@ -4,4 +4,5 @@ from . import views
 app_name = 'order_api'
 urlpatterns = [
     path('cart/', views.CartAPI.as_view(), name='cart'),
+    path('showcart/', views.ShowCartApi.as_view(), name='show_cart')
 ]
