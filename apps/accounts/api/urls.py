307e50045e584edf5575da1668dc01814e4416b4
abolfazl <abolfazlrasouli95@gmail.com>
2024-02-23 18:49:40 +0330

    url implementation for address model

--- apps/accounts/api/urls.py
+++ apps/accounts/api/urls.py
@@ -4,4 +4,6 @@ from . import views
 app_name = 'order_api'
 urlpatterns = [
     path('profile/', views.ProfileAPI.as_view(), name='profile'),
+    path('Addressshoow/', views.AddressApi.as_view(), name='address_show'),
+    path('detailaddress/<int:pk>/', views.DetailAddress.as_view(), name='detail_address')
 ]
